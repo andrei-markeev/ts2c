a2948511112ee12d792806a2afa5623aa1d86ed6
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2020-12-19 17:04:26 +0200

    2.2.9, added import target test

---
+++ tests/statements/import_target.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    printf("%d\n", abs(-5));
+    printf("%d\n", abs(10));
+
+    return 0;
+}
---
+++ tests/statements/import_target.ts
@@ -0,0 +1,4 @@
+import {abs} from "ts2c-target-test/stdlib.h";
+
+console.log(abs(-5));
+console.log(abs(10));
